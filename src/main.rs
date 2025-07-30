71b4d30fbe400fb8f3f7edcd3f75286b72d7898b
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-07-30 20:37:16 +0300

    Added getting json from the api in order to get the link of the video

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use axum::{
-    routing::get,
-    Router,
-};
 use axum::extract::Path;
+use axum::{Router, routing::get};
+use reqwest::{Client, Error, Response};
 
 async fn show_video(Path(link): Path<String>) -> String {
-    return link
+    let http_client = Client::new();
+    // In this function I need to:
+    // * check whether the url passed is valid
+    // * get the downloadable link using the api (post request)
+    let request = http_client
+        .get("https://www.tikwm.com/api/?url=".to_owned() + &link + "&hd=1&web=1")
+        .build();
+    let response = http_client.execute(request.unwrap()).await;
+    match response {
+        Ok(s) => {
+            println!("{}", s.status());
+            println!("{}", s.text().await.unwrap());
+        }
+        Err(e) => {
+            println!("{}", e);
+            return "An error occurred".to_string();
+        }
+    }
+    // * download the video (get request)
+    // * return the video to the client
+
+    return link;
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/{link}", get(show_video));
+    let app = Router::new().route("/{*link}", get(show_video));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
