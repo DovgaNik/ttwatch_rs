877c58cefa241a04c33e9945fd5ab41b0ce92884
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-07-30 18:33:28 +0300

    Figured out a way to get a parameter from http request.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use axum::extract::Path;
+
+async fn show_video(Path(link): Path<String>) -> String {
+    return link
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/{link}", get(show_video));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
