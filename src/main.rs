e81230717ae591de39b48ced13b3ff620707617e
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-07-30 21:13:02 +0300

    Added parsing of the json and redirect to TikTok CDN

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use axum::extract::Path;
-use axum::{Router, routing::get};
-use reqwest::{Client, Error, Response};
+use axum::{Router, extract::Path, routing::get, response::Redirect};
+use reqwest::Client;
+use serde::Deserialize;
 
-async fn show_video(Path(link): Path<String>) -> String {
+// Structs required to parse the JSON returned from the API
+#[derive(Deserialize)]
+struct ApiResponse {
+    data: VideoData,
+}
+
+#[derive(Deserialize)]
+struct VideoData {
+    hdplay: String,
+}
+
+async fn show_video(Path(link): Path<String>) -> Redirect {
     let http_client = Client::new();
     // In this function I need to:
     // * check whether the url passed is valid
@@ -10,21 +21,13 @@ async fn show_video(Path(link): Path<String>) -> String {
     let request = http_client
         .get("https://www.tikwm.com/api/?url=".to_owned() + &link + "&hd=1&web=1")
         .build();
-    let response = http_client.execute(request.unwrap()).await;
-    match response {
-        Ok(s) => {
-            println!("{}", s.status());
-            println!("{}", s.text().await.unwrap());
-        }
-        Err(e) => {
-            println!("{}", e);
-            return "An error occurred".to_string();
-        }
-    }
+    let response = http_client.execute(request.unwrap()).await.unwrap();
+    let json_parsed = response.json::<ApiResponse>().await.unwrap();
+    let hd_link = json_parsed.data.hdplay;
+
+    Redirect::permanent(&format!("https://www.tikwm.com{}", hd_link))
     // * download the video (get request)
     // * return the video to the client
-
-    return link;
 }
 
 #[tokio::main]
